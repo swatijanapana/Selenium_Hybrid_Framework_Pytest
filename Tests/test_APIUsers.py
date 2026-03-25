4df3a50fa5a72b763f9cb795faffe6601e0bf611
swatij777 <swati.janapana@gmail.com>
2026-03-24 20:14:23 -0500

    Added API test cases for GET and POST operations

---
+++ Tests/test_APIUsers.py
@@ -0,0 +1,83 @@
+from Utilities.api_client import APIClient
+
+
+class Test_APIUsers():
+
+    def setup_method(self):
+        self.api_client = APIClient()
+
+    def test_get_users_list(self):
+        """ Verify users API returns 200 and valid data. """
+
+        url = "https://jsonplaceholder.typicode.com/users"
+        response = self.api_client.get(url)
+
+        assert response.status_code == 200
+        response_json = response.json()
+        assert len(response_json) > 0
+        assert "id" in response_json[0]
+        assert "email" in response_json[0]
+
+
+
+    def test_create_user(self):
+        """ Verify user can be created through POST API. """
+
+        url = "https://jsonplaceholder.typicode.com/users"
+
+        payload = {"name": "Test",
+                   "email": "api@test.com"}
+
+        response = self.api_client.post(url, payload)
+        assert response.status_code == 201
+        response_json = response.json()
+        assert response_json["name"] == "Test"
+        assert response_json["email"] == "api@test.com"
+
+
+
+    def test_create_user_with_missing_fields(self):
+        """ Verify API behaviour when required fields are missing. """
+
+        url = "https://jsonplaceholder.typicode.com/users"
+
+        payload = {"name": ""} # missing email
+        response = self.api_client.post(url, payload)
+        # API still returns 201 (fake API behavior)
+        assert response.status_code == 201
+        response_json = response.json()
+        assert "email" not in response_json or response_json.get("email") in [None, ""]
+
+
+
+    def test_invalid_endpoint(self):
+        """ Verify users API returns 404. """
+
+        url = "https://jsonplaceholder.typicode.com/invalid"
+
+        response = self.api_client.get(url)
+        assert response.status_code == 404
+
+
+    def test_update_user(self):
+        """Verify user data can be updated using PUT API. """
+
+        url = "https://jsonplaceholder.typicode.com/users/1"
+
+        payload = {"name": "Updated Test",
+                   "email": "updated_api@test.com"}
+
+        response = self.api_client.put(url, payload)
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["name"] == "Updated Test"
+        assert response_json["email"] == "updated_api@test.com"
+
+    def test_delete_user(self):
+        """Verify user can be deleted using DELETE API."""
+
+        url = "https://jsonplaceholder.typicode.com/users/1"
+
+        response = self.api_client.delete(url)
+        assert response.status_code == 200
+
